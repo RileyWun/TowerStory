f08b8dba4683f013ef3ae8b7bbcd78f74fc84451
RileyWun <143245498+RileyWun@users.noreply.github.com>
2025-06-04 11:28:52 +1000

    Create npc2-quest-tree.js

---
+++ assets/js/dialogues/npc2-quest-tree.js
@@ -0,0 +1,38 @@
+// assets/js/dialogues/npc2-quest-tree.js
+
+export default {
+  start: 'greeting',
+  nodes: {
+    greeting: {
+      type: 'text',
+      lines: ['Hello traveler! Will you help me?'],
+      next: 'offerQuest'
+    },
+    offerQuest: {
+      type: 'choice',
+      question: 'Will you accept this quest?',
+      options: [
+        {
+          text: 'Yes, I will accept this quest',
+          action: () => {
+            console.log('NPC2 quest “Yes” clicked');
+            this.scene.questManager.accept('quest2');
+          },
+          next: 'questAccepted'
+        },
+        {
+          text: 'Not right now',
+          next: 'farewell'
+        }
+      ]
+    },
+    questAccepted: {
+      type: 'text',
+      lines: ['Thank you! Here are 5 coins.']
+    },
+    farewell: {
+      type: 'text',
+      lines: ['Safe travels, then.']
+    }
+  }
+};
